9978d23906f8258923f9139e2eda4eee66a57590
jiangheqing.jhq <jiangheqing.jhq@alibaba-inc.com>
2025-11-28 09:32:56 +0800

    Fix license and format
    Change-Id: I75271c097084994718c3bdff6eb690a0ae888a1b

--- src/main/java/io/agentscope/core/tracing/AttributesExtractors.java
+++ src/main/java/io/agentscope/core/tracing/AttributesExtractors.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.agentscope.core.tracing;
 
 import static io.agentscope.core.tracing.AgentScopeIncubatingAttributes.AGENTSCOPE_FORMAT_TARGET;
@@ -46,7 +62,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.agentscope.core.agent.AgentBase;
 import io.agentscope.core.formatter.AbstractBaseFormatter;
-import io.agentscope.core.formatter.anthropic.AnthropicBaseFormatter;
 import io.agentscope.core.formatter.anthropic.AnthropicChatFormatter;
 import io.agentscope.core.formatter.anthropic.AnthropicMultiAgentFormatter;
 import io.agentscope.core.formatter.dashscope.DashScopeChatFormatter;
