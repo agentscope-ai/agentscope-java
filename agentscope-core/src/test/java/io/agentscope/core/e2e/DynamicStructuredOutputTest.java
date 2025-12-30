6efe52103ded07c024e560713c526ff91517b177
jiangzhen <jiangzhen0929@163.com>
2025-12-30 20:45:08 +0800

    add license header

--- agentscope-core/src/test/java/io/agentscope/core/e2e/DynamicStructuredOutputTest.java
+++ agentscope-core/src/test/java/io/agentscope/core/e2e/DynamicStructuredOutputTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.agentscope.core.e2e;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -23,6 +39,19 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
+/**
+ * Consolidated E2E tests for structured output functionality.
+ *
+ * <p>Tests structured output generation across various scenarios including:
+ * <ul>
+ *   <li>Dynamic complex nested data structures</li>
+ * </ul>
+ *
+ * <p><b>Requirements:</b> OPENAI_API_KEY and/or DASHSCOPE_API_KEY environment variables
+ * must be set. Tests are dynamically enabled based on available API keys and model capabilities.
+ */
 
 @Tag("e2e")
 @Tag("dynamic-structured-output")
