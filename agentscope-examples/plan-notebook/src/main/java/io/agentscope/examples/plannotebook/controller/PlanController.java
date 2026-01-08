e0911dbf1e5d64c0fe5707c7710da2c4ce47c7c3
LearningGp <learninggp4.74@gmail.com>
2026-01-08 20:26:40 +0800

    chore: reformat
    Change-Id: I116b188a5fe5cab9d15af2c0406b931d48fd8afc

--- agentscope-examples/plan-notebook/src/main/java/io/agentscope/examples/plannotebook/controller/PlanController.java
+++ agentscope-examples/plan-notebook/src/main/java/io/agentscope/examples/plannotebook/controller/PlanController.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +32,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
