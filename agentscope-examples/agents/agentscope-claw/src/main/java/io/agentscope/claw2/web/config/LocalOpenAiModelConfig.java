0dd0ef87425a8e5a5dddb27381b8da9a37b58548
lancefoever <luzkingforever@gmail.com>
2026-06-02 08:44:22 +0800

    chore(claw): add license headers

--- agentscope-examples/agents/agentscope-claw/src/main/java/io/agentscope/claw2/web/config/LocalOpenAiModelConfig.java
+++ agentscope-examples/agents/agentscope-claw/src/main/java/io/agentscope/claw2/web/config/LocalOpenAiModelConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package io.agentscope.claw2.web.config;
 
@@ -39,18 +54,20 @@ public class LocalOpenAiModelConfig {
                         ? properties.connectTimeout()
                         : Duration.ofSeconds(10);
         Duration readTimeout =
-                properties.readTimeout() != null ? properties.readTimeout() : Duration.ofSeconds(120);
+                properties.readTimeout() != null
+                        ? properties.readTimeout()
+                        : Duration.ofSeconds(120);
 
         log.info(
-                "Building local OpenAI-compatible model: baseUrl={}, endpointPath={}, model={}, stream={}",
+                "Building local OpenAI-compatible model: baseUrl={}, endpointPath={}, model={},"
+                        + " stream={}",
                 properties.baseUrl(),
                 properties.endpointPath(),
                 properties.modelName(),
                 properties.stream());
 
         GenerateOptions.Builder options =
-                GenerateOptions.builder()
-                        .stream(properties.stream())
+                GenerateOptions.builder().stream(properties.stream())
                         .executionConfig(
                                 ExecutionConfig.builder()
                                         .timeout(readTimeout)
